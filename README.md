a5ade3aba429f72e3765de35051798e5cbb4920e
blueflyingpanda <m.s.v.inkognito@gmail.com>
2022-11-26 15:13:42 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # MergeVideos
-retrieves many small videos in ts format and merges it into one mp4 video
+Retrieves many small videos in ts format from urls in video_urls.txt and merges them into one mp4 video
---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import subprocess
+from tqdm import tqdm
+from pathlib import Path
+from typing import Generator
+
+
+PARTS_DIR = 'video_parts'
+PARTS_FILE = 'ts_files.txt'
+Path(PARTS_DIR).mkdir(exist_ok=True)
+
+
+def get_urls(from_file: str) -> Generator[str]:
+    with open(from_file) as fr:
+        text = fr.readlines()
+    return (line.strip() for line in text if line.startswith('https://'))
+
+
+def download_video_parts(to_directory: str, urls: Generator[str]) -> int:
+    """
+
+    :param to_directory: where to save
+    :param urls: from where to download
+    :return: amount of files
+    """
+    i = 0
+    for url in tqdm(urls):
+        response = requests.get(url)
+        with open(f'{to_directory}/{i}.ts', 'wb') as fwb:
+            fwb.write(response.content)
+        i += 1
+    return i
+
+
+def list_parts(in_file: str, number_of_parts: int) -> None:
+    with open(in_file, 'w') as fw:
+        n = 0
+        while n < number_of_parts:
+            fw.write(f"file '{PARTS_DIR}/{n}.ts'\n")
+            n += 1
+
+
+if __name__ == '__main__':
+
+    urls = get_urls(from_file='video_urls.txt')
+    number_of_parts = download_video_parts(to_directory=PARTS_DIR, urls=urls)
+    list_parts(in_file=PARTS_FILE, number_of_parts=number_of_parts)
+
+    subprocess.run(['ffmpeg', '-f', 'concat', '-i', PARTS_FILE, '-c', 'copy', 'video.mp4'])
